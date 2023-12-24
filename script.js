d06e39cc67c5a75613f542824ec14aa0a26cadfe
Syamil Ali <68064849+davious-cpu@users.noreply.github.com>
2023-12-24 14:38:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function toggleMenu(){
+    
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    
+    // add or remove (toggle) class open to the selector
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+
+}
+
+
+/*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener('click', ()=>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
